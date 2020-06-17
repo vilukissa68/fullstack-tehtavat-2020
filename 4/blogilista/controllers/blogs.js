eee54ad73b460aad8b6af7fd0a78737cab7b588b
vaino <vaino.granat1@gmail.com>
2020-06-17 16:38:22 +0300

    Finished all exercises in section 4b.

--- 4/blogilista/controllers/blogs.js
+++ 4/blogilista/controllers/blogs.js
@@ -2,14 +2,14 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-blogsRouter.get('/', async (request, response, next) => {
+blogsRouter.get('/', async (request, response) => {
 
   const blogs = await Blog.find({})
   response.json(blogs.map(blog => blog.toJSON()))
 
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
 
   const body = request.body
 
@@ -22,6 +22,27 @@ blogsRouter.post('/', async (request, response, next) => {
 
   const savedBlog = await blog.save()
   response.json(savedBlog.toJSON())
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  console.log("Put request:")
+  const body = request.body
+
+  const updatedBlog = {
+    title: body.title,
+    author: body.title,
+    url: body.url,
+    likes: body.likes ? body.likes : 0
+  }
+
+  const blog = await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true})
+  console.log(blog)
+  response.json(blog)
 })
 
 module.exports = blogsRouter
