7f49ae0634a5c7f0d75c961236ef62fa9356fa3e
vaino <vaino.granat1@gmail.com>
2020-06-23 17:55:28 +0300

    Finished exercises 4.17 and 4.18.

--- 4/blogilista/controllers/blogs.js
+++ 4/blogilista/controllers/blogs.js
@@ -1,10 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-
+const User = require('../models/user')
+const helper = require('../utils/list_helpers')
 
 blogsRouter.get('/', async (request, response) => {
 
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1})
   response.json(blogs.map(blog => blog.toJSON()))
 
 })
@@ -13,14 +14,20 @@ blogsRouter.post('/', async (request, response) => {
 
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes ? body.likes : 0
+    likes: body.likes ? body.likes : 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.json(savedBlog.toJSON())
 })
 
