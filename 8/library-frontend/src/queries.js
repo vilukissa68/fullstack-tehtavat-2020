ad022687a8447cd1e6bd8bae6d73df8606f82615
vaino <vaino.granat1@gmail.com>
2020-08-06 20:21:01 +0300

    Finished exercises 8.8 and 8.9.

---
+++ 8/library-frontend/src/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      bookCount
+      born
+    }
+  }
+`
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`
